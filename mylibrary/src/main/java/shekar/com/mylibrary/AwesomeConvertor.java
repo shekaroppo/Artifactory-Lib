a3c0cd101ab315768a24a831c540c00557f181b6
Shekar <shekarrex@gmail.com>
2016-07-31 14:08:22 +0530

    Added converter class which will be used in shell(Artifactory-Sample) repo

---
+++ mylibrary/src/main/java/shekar/com/mylibrary/AwesomeConvertor.java
@@ -0,0 +1,11 @@
+package shekar.com.mylibrary;
+
+/**
+ * Created by Shekar on 7/31/16.
+ */
+public class AwesomeConvertor {
+
+    public static String toAwesome(String nonAwesomeString) {
+        return nonAwesomeString.replaceAll("\\w+", "Awesome!");
+    }
+}
